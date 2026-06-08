b329c5ef14f4625d915a1835916986f7f08cfbfa
Riya Chhabra <richhabra@ucsd.edu>
2026-06-07 22:34:32 -0700

    Added trypsinize() function

--- flufinder.R
+++ flufinder.R
@@ -6,3 +6,9 @@ upload_fasta <- function(fasta_filename) {
              as.string = TRUE,
              set.attributes = FALSE)
 }
+
+trypsinize <- function(proteins) {
+  library(stringr)
+  
+  lapply(proteins, str_split_1, pattern = "(?<=R|K)")
+}
